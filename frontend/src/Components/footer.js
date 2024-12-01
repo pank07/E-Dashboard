ddb295cec35ca656a1684a6406a7bebc211eda70
pank07 <159031153+pank07@users.noreply.github.com>
2024-12-01 19:52:50 +0530

    footer.js

--- frontend/src/Components/footer.js
+++ frontend/src/Components/footer.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import {ReactComponent as Facebook} from './facebook-brands-solid.svg';
+import {ReactComponent as Twitter } from './square-x-twitter-brands-solid.svg';
+import {ReactComponent as Whatsapp } from './square-whatsapp-brands-solid.svg';
+import {ReactComponent as Telegram } from './telegram-brands-solid.svg';
+import {ReactComponent as Insta } from './square-instagram-brands-solid.svg';
+import {ReactComponent as Telephone } from './phone-solid.svg';
 
 function Footer() {
   return (
-    <div className='footer'>@Copyright2024
-    <ul>
-      <li id='whatsapp'><a href='/'>Whatsapp</a></li>
-      <li id='Facebook'><a href='/'>Facebook</a></li>
-      <li id='instagram'><a href='/'>instagram</a></li>
-      <li id='Telegram'><a href='/'>Telegram</a></li>
-      <li id='Twitter'><a href='/'>Twitter</a></li>
-      <li id='telephone'>+91-8725898258</li>
+    <div className='footer'>Copyright © 2024 E-dashboard. All rights are reserved
+    <ul className='social'>
+      <li id='whatsapp'><a href='/'>
+      <Whatsapp width='50' height='50' fill='Red'/></a></li>
 
+      <li id='Facebook'><a href='/'>
+      <Facebook width='50' height='50' fill='blue'/>
+      </a></li>
+
+      <li id='Twitter'><a href='/'>
+      <Twitter width='50' height='50' fill='Black'/>
+      </a></li>
+
+      <li id='instagram'><a href='/'>
+      <Insta width='50' height='50' fill='red'/></a></li>
+
+      <li id='Telegram'><a href='/'>
+      <Telegram width='50' height='50' fill='Black'/>
+      </a></li>
+
+      <li id='telephone'>
+      <Telephone width='25' height='25' fill='Black'/>
+      +91-8725898258</li>
       </ul>
-
-
     </div>
 );
 }
