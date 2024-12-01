66ec0155b893dbedb3df38f3410da4d942e96aa2
pank07 <159031153+pank07@users.noreply.github.com>
2024-12-01 19:50:59 +0530

    Nav.js

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as Profile } from './user-solid.svg';
+import { ReactComponent as Logout } from './right-from-bracket-solid.svg';
+
 
 function Nav() {
   const auth = localStorage.getItem('user');
@@ -12,7 +15,7 @@ function Nav() {
   return (
     <div className='container-fluid'>
       <nav className='wel-text'>
-        {/* <marquee>Welcome to E-dashboard</marquee> */}
+        <marquee>Welcome to E-dashboard</marquee>
         {
           auth ? (
             <ul className='navbar mt-5'>
@@ -21,7 +24,10 @@ function Nav() {
               <li><Link to="/update/:id">Update Product</Link> </li>
               {/* <li><Link to="/about">About us</Link> </li>
               <li><Link to="/contact">Contact us</Link> </li> */}
-              <li><Link onClick={logout} to="/signup">Logout</Link></li>
+              <li><Link onClick={logout} to="/signup">Logout
+              </Link>
+              <Logout width="25" height="20" fill='black'/>
+              </li>
             </ul>) : (
             <ul className='navbar2'>
               {/* <li><Link to="/signup">Sign Up</Link></li> */}
@@ -30,7 +36,10 @@ function Nav() {
         }
       </nav>
       <img className="logo" alt='logo' src='./logo.jpg' />
-      <div>{auth && <span className='text-warning mx-5 fw-bold fs-3'>Welcome,{JSON.parse(auth).name}!</span>}</div>
+
+      <div>{auth && <span className='text-light mx-5 fw-bold fs-3'>
+        <Profile id="profile" width="50" height="25" fill='black' style={{marginTop: "20px", marginBottom:"30px" }} />
+        Welcome,{JSON.parse(auth).name}!</span>}</div>
 
     </div>
   )
