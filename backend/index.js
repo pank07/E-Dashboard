ffdf4a36a959c6a0aa3c47b685f13a1e9a4cc957
pank07 <159031153+pank07@users.noreply.github.com>
2024-12-13 01:18:49 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+
+// const mongoose= require('mongoose');
+// MongoDB Cloud Atlas connection URI
+// const MONGO_URI = 'mongodb+srv://pankajsonwani:pankajMongodb07@pankaj-db.dutpt.mongodb.net/?retryWrites=true&w=majority&appName=pankaj-DB';
+
+// Connect to MongoDB Atlas
+// mongoose.connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+//     .then(() => console.log('Connected to MongoDB Atlas'))
+//     .catch((error) => console.error('Error connecting to MongoDB:', error));
 require('./db/config');
 const User = require('./db/User');
 const Product = require('./db/Product');
-
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-// app.post("/register", async (req, res) => {
-//     let user = new User(req.body);
-//     let result = await user.save();
-//     result =result.toObject();
-//     delete result.password
-//     res.send(result);
-// })
-
-app.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
-
-    if (!username || !email || !password) {
-        return res.status(400).send('All fields are required.');
-    }
-
-    // Continue with user creation logic
-    // Example: Save user to database
-});
-
+app.post("/register", async (req, res) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
+})
 
 app.post('/login', async (req, res) => {
     console.log(req.body)
@@ -43,42 +38,42 @@ app.post('/login', async (req, res) => {
 }
 );
 
-app.post('/add-product',async(req,res)=>{
-    let product= new Product(req.body);
-    let result= await product.save();
-    res.send(result); 
+app.post('/add-product', async (req, res) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    res.send(result);
 })
 
-app.get('/products',async(req,res)=>{
-    const products =await Product.find();
-    if(products.length>0){
+app.get('/products', async (req, res) => {
+    const products = await Product.find();
+    if (products.length > 0) {
         res.send(products)
-    }else{
-        res.send({result:"No product found"})
+    } else {
+        res.send({ result: "No product found" })
     }
 });
 
-app.delete("/product/:id",async(req,res)=>{
-    let result=await Product.deleteOne({_id:req.params.id});
+app.delete("/product/:id", async (req, res) => {
+    let result = await Product.deleteOne({ _id: req.params.id });
     res.send(result)
 }),
-app.get("/product/:id",async(req,res)=>{
-    let result =await Product.findOne({_id:req.params.id})
-    if(result){
-        res.send(result)
-    }else{
-        res.send({"result":"No record Found"})
-    }
-})
+    app.get("/product/:id", async (req, res) => {
+        let result = await Product.findOne({ _id: req.params.id })
+        if (result) {
+            res.send(result)
+        } else {
+            res.send({ "result": "No record Found" })
+        }
+    })
 
-app.put("/product/:id",async(req,res)=>{
+app.put("/product/:id", async (req, res) => {
     let result = await Product.updateOne(
-        {_id:req.params.id},
-        {$set:req.body}
+        { _id: req.params.id },
+        { $set: req.body }
     )
     res.send(result)
 });
- 
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`server is running http://localhost:${PORT}`)
